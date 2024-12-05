7dc26e1f604f69d9717cb3233aec68eb757403db
FredPont <fred.softwares@gmail.com>
2024-12-05 18:27:10 +0100

    Fix file name bug

--- src/fileutil/filename.go
+++ src/fileutil/filename.go
@@ -3,16 +3,27 @@ package fileutil
 import (
 	conf "FileFootprintLister/src/configuration"
 	"fmt"
+	"os"
 	"regexp"
+	"strings"
 )
 
 func FormatName(name string) string {
 	rules := conf.ReadRules()
+	// get the last element from the path
+	if hasPathSeparators(name) {
+		_, name = GetFileAndPath(name)
+	}
 
 	for _, rule := range rules {
 		name = replaceChar(rule, name)
 	}
 	return name
+}
+
+// hasPathSeparators checks if the given string contains path separators.
+func hasPathSeparators(path string) bool {
+	return strings.Contains(path, string(os.PathSeparator)) || strings.Contains(path, "/")
 }
 
 func replaceChar(rules []string, text string) string {
