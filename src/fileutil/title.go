26429eac99a75dca64c75103500ef8a7c51cdb92
FredPont <fred.softwares@gmail.com>
2025-05-06 11:30:56 +0200

    change version

--- src/fileutil/title.go
+++ src/fileutil/title.go
@@ -24,7 +24,7 @@ func Title() {
 	fmt.Println("")
 	fmt.Println("   ┌────────────────────────────────────────┐") // unicode U+250C
 	fmt.Println("   │  FileFootprintLister (c)Frederic PONT  │")
-	fmt.Println("   │    v20250120 - Free Software GNU GPL   │")
+	fmt.Println("   │    v20250506 - Free Software GNU GPL   │")
 	fmt.Println("   └────────────────────────────────────────┘")
 	//fmt.Println("")
 }
